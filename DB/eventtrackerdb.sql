2bc45c16554c4b03bafd1ac2dcc253195850a405
sethschn <sethschn301@gmail.com>
2020-04-10 15:31:29 -0600

    Initial db commit

---
+++ DB/eventtrackerdb.sql
@@ -0,0 +1,48 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema eventtrackerdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `eventtrackerdb` ;
+
+-- -----------------------------------------------------
+-- Schema eventtrackerdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `eventtrackerdb` DEFAULT CHARACTER SET utf8 ;
+USE `eventtrackerdb` ;
+
+-- -----------------------------------------------------
+-- Table `dummy`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `dummy` ;
+
+CREATE TABLE IF NOT EXISTS `dummy` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS etuser@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'etuser'@'localhost' IDENTIFIED BY 'etuser';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'etuser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `dummy`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eventtrackerdb`;
+INSERT INTO `dummy` (`id`, `name`) VALUES (1, 'Frog');
+
+COMMIT;
+
